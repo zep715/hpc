60c91347ea75770ecee27ef43f094f1021ccdd1c
zep715 <zep715@gmail.com>
2017-06-21 17:43:44 +0200

    Update apriori.cpp

--- apriori.cpp
+++ apriori.cpp
@@ -1,170 +1,172 @@
 #include <iostream>
-#include <cstdlib>
 #include <fstream>
+#include <string>
+#include <sstream>
 #include <vector>
 #include <set>
 #include <map>
+#include <cstdlib>
+#include <chrono>
 #include <algorithm>
-
 using namespace std;
-
-ostream &operator<<(ostream &os, set<string> &s) {
-	for (string i : s) {
-		os << i << " ";
-	}
-    	return os;
-}
-
-ostream &operator<<(ostream &os, set<set<string> > &s) {
-	for (set<string> i: s) {
-		os << i << endl;
-	}
-	return os;
-}
+using ns = chrono::nanoseconds;
+using get_time = chrono::steady_clock ;
 
 
-void load_transactions(ifstream &file, vector<set<string> > &transactions) {
+
+void load_transactions(ifstream &input, vector<vector<int> > &transactions, int &m) {
 	string s;
-	while (file >> s) {
-		set<string> t;
+	vector<int> temp;
+	int max = 0;
+	while(input >> s) {
+		temp.clear();
 		string::iterator current = s.begin();
 		for (string::iterator i = s.begin(); i != s.end(); i++) {
 		    if (*i == ',') {
-		        t.insert(string(current, i));
+			temp.push_back(atoi(string(current,i).c_str()));
 		        current = i+1;
 		    }
 		    if (i == s.end()-1) {
-		        t.insert(string(current, i+1));
+			temp.push_back(atoi(string(current,i+1).c_str()));
 		    }
 		}
-		transactions.push_back(t);
-    	}
+		transactions.push_back(temp);
+		if (temp.back() >= max)
+			max = temp.back();
+	}
+	m = max;
+
 
 }
 
-void get_1itemset(vector<set<string> > &transactions,float min_sup,set<set<string> > &candidates) {
 
-	map<string, float> counter;
-	for (set<string> i : transactions) {
-		for(string l : i) {
-		    if(!counter.count(l)) {
-			counter[l] = 1.0;
-		    } else {
-			counter[l] +=1.0;
-		    } 
+
+void get_1itemset(int max_id, float min_sup, vector<vector<int> > &transactions, vector<vector<int> > &first_itemset) {
+	int *counter = new int[max_id]();
+	unsigned int N = transactions.size();
+	for(unsigned int i = 0; i < N; i++) {
+		for (unsigned int j = 0; j < transactions[i].size(); j++) {
+			counter[transactions[i][j]] += 1;
 		}
 	}
-	for(auto& kv : counter) {
-		float support = kv.second / transactions.size();
-		kv.second = support;
-	}
-	for (auto& x : counter) {
-		if (x.second >= min_sup) {
-			set<string> temp;
-			temp.insert(x.first);
-			candidates.insert(temp);
+	vector<int> temp;
+	for (int i = 0; i < max_id; i++) {
+		temp.clear();
+		if ( (((float)counter[i]) / N) >= min_sup) {
+			temp.push_back(i);
+			first_itemset.push_back(temp);
 		}
+			
 	}
-
-
-
+	delete[] counter;
 }
-void join_set(set<set<string> > &s, int l, set<set<string> > &cands) {
-	cands.clear();
-	set<string> temp;
-	for (set<string> i: s) {
-		for(set<string> j:s) {
-			temp.clear();
-			temp = i;
-			temp.insert(j.begin(), j.end()); //set union
-			if (temp.size() == l) {
-				cands.insert(temp);
+
+void itemsets_min_support(vector<vector<int> > &transactions, vector<vector<int> > &candidates, float min_sup, vector<vector<int> > &result) {
+	int *counter = new int[candidates.size()]();
+	unsigned int N = transactions.size();
+	for (unsigned int i = 0; i < transactions.size(); i++) {
+		for (unsigned int j = 0; j < candidates.size(); j++) {
+			//if every element in cadidates[j] is in transactions[i], then counter[j]++
+			bool not_found = false;			
+			for(unsigned int k = 0; k < candidates[j].size(); k++) {
+				if (!binary_search(transactions[i].begin(), transactions[i].end(), candidates[j][k])) {
+					not_found = true;
+					break;
+				}
 			}
+			if (!not_found) {
+				counter[j]++;
+			}	
+		}
+	}
+	for (unsigned int i = 0; i < candidates.size(); i++) {
+		if ( (((float)counter[i]) / N) >= min_sup) {
+			result.push_back(candidates[i]);
+		}
+	}
+	delete[] counter;
+}
+
+void first_candidates(vector<int> &first_itemset, vector<vector<int> > &candidates) {
+	
+	vector<int> temp;
+	for (unsigned int i = 0; i < first_itemset.size(); i++) {
+		
+		for (unsigned int j = i+1; j < first_itemset.size(); j++) {
+			temp.clear();
+			temp.push_back(first_itemset[i]);
+			temp.push_back(first_itemset[j]);			
+			candidates.push_back(temp);
+			
+		}
+	}	
+
+
+}
+
+void join_set( vector<vector<int> > &itemset, int k, vector<vector<int> > &candidates) {
+	if (k == 2) {
+		vector<int> temp;
+		for (unsigned int i = 0; i < itemset.size(); i++) {
+			for (unsigned int j = i+1; j < itemset.size(); j++) {
+				temp.clear();
+				temp.push_back(itemset[i][0]);
+				temp.push_back(itemset[j][0]);
+				candidates.push_back(temp);
+			}
+		}
+		return;
+	}
+	int len = k-2;
+	vector<int> temp;
+	for (unsigned int i = 0; i < itemset.size(); i++) {
+		for (unsigned int j = 0; j < itemset.size(); j++) {
+			bool joinable = true;
+			temp.clear();
+			for (int k = 0; k < len; k++) {
+				if (itemset[i][k] != itemset[j][k]) {
+					joinable = false;
+					break;
+				}
+			}
+			if (!joinable)
+				continue;
+			if (itemset[i].back() == itemset[j].back()) {
+				continue;
+			}
+			temp.assign( itemset[i].begin(), itemset[i].end()-1 );
+			if (itemset[i].back() >= itemset[j].back()) {
+				temp.push_back(itemset[j].back());
+				temp.push_back(itemset[i].back());
+			} else {
+				temp.push_back(itemset[i].back());
+				temp.push_back(itemset[j].back());
+			}
+			candidates.push_back(temp);
 			
 		}
 	}
 
 }
-//s.issubset(t)
-//da migliorare
-template<typename T>
-bool is_subset(T s_begin, T s_end, T t_begin, T t_end) {
-	bool result = true;
-	for (T it = s_begin; it != s_end; it++) {
-		bool temp = false;
-		for (T it2 = t_begin; it2 != t_end; it2++) {
-			if (*it == *it2) {
-				temp = true;
-				break;
-			}
-		}
-		result &= temp;
-	}
-	
-	return result;
-}
-void itemsets_min_support(vector<set<string> > &transactions, set<set<string> > &candidates,  map<set<string>, float> &counter, float min_sup) {
-	counter.clear();
-	for (set<string>  t : transactions) {
-		for (set<string>  c: candidates) {
-			if(is_subset(c.begin(), c.end(), t.begin(), t.end())) {
-				if(!counter.count(c)) {
-					counter[c] = 1.0;
-				} else {
-					counter[c] += 1.0;
-				}
-			}
-		}
-	}
-	for (auto& kv : counter) {
-		float support = kv.second / transactions.size();
-		kv.second = support;
-
-	}
-	
-	
-	for (auto it = counter.cbegin(), next_it = counter.cbegin(); it != counter.cend(); it = next_it) {
-		 next_it = it; ++next_it;
-		if(it->second < min_sup) {
-			counter.erase(it);
-		}
-	}
-}
-
-
 int main(int argc, char *argv[]) {
-	if (argc < 4) {
+	if (argc != 4) {
 		cout << "usage: ./apriori file support confidence" << endl;
 		return 1;
 	}
-	float min_sup = atof(argv[2]);
-	float min_conf = atof(argv[3]);
-	vector<set<string> > transactions;
-	set< set<string> > large_itemsets;
+	int max_id;
+	float min_sup, min_conf;
+	vector<vector<int> > transactions;
+	vector<int> first_itemset;
+	ifstream input;
 	
-	ifstream input(argv[1], ios::in);
-	load_transactions(input, transactions);
+	min_sup = atof(argv[2]);
+	min_conf = atof(argv[3]);
+	input.open(argv[1], ios::in);
 	
-	set<set<string> >candidates;
-	get_1itemset(transactions, min_sup, candidates);
-	large_itemsets.insert(candidates.begin(), candidates.end());
-	set<set<string> >current_litemset = candidates;
-	map<set<string>, float> counter;
-
-	for (int k = 2; !current_litemset.empty(); k++) {
-		join_set(candidates,k, current_litemset);
-		itemsets_min_support(transactions,current_litemset,counter,min_sup);
-		current_litemset.clear();
-		for (auto &kv : counter) {
-			current_litemset.insert(kv.first);
-			
-		}
-		large_itemsets.insert(current_litemset.begin(), current_litemset.end());
-		candidates.clear();
-		candidates = current_litemset;
-		
-		
-	}
-	cout << large_itemsets << endl;
-
+	load_transactions(input, transactions, max_id);
+	input.close();
+	
+	get_1itemset(max_id, min_sup, transactions, first_itemset);
+	
+	
 }
