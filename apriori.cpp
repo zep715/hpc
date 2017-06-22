432adc3944bd2fbdd085fd93e90ff9e7fbf806a4
zep715 <zep715@gmail.com>
2017-06-22 17:07:46 +0200

    Update apriori.cpp

--- apriori.cpp
+++ apriori.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <vector>
 #include <set>
 #include <map>
 #include <cstdlib>
-#include <chrono>
 #include <algorithm>
 using namespace std;
-using ns = chrono::nanoseconds;
-using get_time = chrono::steady_clock ;
 
 
+void print_set(vector<vector<int> > &s, const string title) {
+	cout << title << endl;
+	for(vector<int> &x : s) {
+		for (int y : x) { 
+			cout << y << " ";
+		}
+		cout << ";";
+	}
+	cout << endl;
 
+}
+
+void print_largeitemset(map<vector<int>, float> &large_itemsets) {
+	cout << "Large itemsets with min support:" << endl;
+	for (auto &kv : large_itemsets) {
+		cout << "(";
+		vector<int>::const_iterator it;
+		for (it = kv.first.begin(); it != kv.first.end()-1; ++it)  {
+			cout << *it << ",";
+		}
+	
+		cout << *(it) << "): " << kv.second << endl;
+	}
+	cout << "------------------------" << endl;
+
+}
 void load_transactions(ifstream &input, vector<vector<int> > &transactions, int &m) {
 	string s;
 	vector<int> temp;
@@ -41,7 +62,7 @@ void load_transactions(ifstream &input, vector<vector<int> > &transactions, int 
 
 
 
-void get_1itemset(int max_id, float min_sup, vector<vector<int> > &transactions, vector<vector<int> > &first_itemset) {
+void get_1itemset(int max_id, float min_sup, vector<vector<int> > &transactions, vector<vector<int> > &first_itemset, map<vector<int>, float> &large_itemsets) {
 	int *counter = new int[max_id]();
 	unsigned int N = transactions.size();
 	for(unsigned int i = 0; i < N; i++) {
@@ -55,13 +76,14 @@ void get_1itemset(int max_id, float min_sup, vector<vector<int> > &transactions,
 		if ( (((float)counter[i]) / N) >= min_sup) {
 			temp.push_back(i);
 			first_itemset.push_back(temp);
+			large_itemsets[temp] = ((float)counter[i]) / N;
 		}
 			
 	}
 	delete[] counter;
 }
 
-void itemsets_min_support(vector<vector<int> > &transactions, vector<vector<int> > &candidates, float min_sup, vector<vector<int> > &result) {
+void itemsets_min_support(vector<vector<int> > &transactions, vector<vector<int> > &candidates, float min_sup, vector<vector<int> > &result, map<vector<int>, float> &large_itemsets) {
 	int *counter = new int[candidates.size()]();
 	unsigned int N = transactions.size();
 	for (unsigned int i = 0; i < transactions.size(); i++) {
@@ -82,27 +104,12 @@ void itemsets_min_support(vector<vector<int> > &transactions, vector<vector<int>
 	for (unsigned int i = 0; i < candidates.size(); i++) {
 		if ( (((float)counter[i]) / N) >= min_sup) {
 			result.push_back(candidates[i]);
+			large_itemsets[candidates[i]] = ((float)counter[i]) / N;
 		}
 	}
 	delete[] counter;
 }
 
-void first_candidates(vector<int> &first_itemset, vector<vector<int> > &candidates) {
-	
-	vector<int> temp;
-	for (unsigned int i = 0; i < first_itemset.size(); i++) {
-		
-		for (unsigned int j = i+1; j < first_itemset.size(); j++) {
-			temp.clear();
-			temp.push_back(first_itemset[i]);
-			temp.push_back(first_itemset[j]);			
-			candidates.push_back(temp);
-			
-		}
-	}	
-
-
-}
 
 void join_set( vector<vector<int> > &itemset, int k, vector<vector<int> > &candidates) {
 	if (k == 2) {
@@ -155,8 +162,8 @@ int main(int argc, char *argv[]) {
 	}
 	int max_id;
 	float min_sup, min_conf;
-	vector<vector<int> > transactions;
-	vector<int> first_itemset;
+	vector<vector<int> > transactions, first_itemset, candidates, current_itemset;
+	map<vector<int>, float> large_itemsets;
 	ifstream input;
 	
 	min_sup = atof(argv[2]);
@@ -166,7 +173,26 @@ int main(int argc, char *argv[]) {
 	load_transactions(input, transactions, max_id);
 	input.close();
 	
-	get_1itemset(max_id, min_sup, transactions, first_itemset);
-	
-	
+	get_1itemset(max_id, min_sup, transactions, first_itemset, large_itemsets);
+	current_itemset = first_itemset;
+	//print_set(first_itemset, "fisrst itemset");
+	for (int k = 2; ; k++) {
+		candidates.clear();
+		join_set(current_itemset, k, candidates);
+		//print_set(candidates,"candidates");
+		if (candidates.empty())
+			break;
+		current_itemset.clear();
+		itemsets_min_support(transactions, candidates, min_sup, current_itemset, large_itemsets);
+		//print_set(current_itemset,"candidates with min support");
+		
+	}
+	candidates.clear();
+	print_largeitemset(large_itemsets);
+	for (const auto &kv: large_itemsets) {
+		candidates.push_back(kv.first);
+	}
+	for (unsigned int i = 0; i < candidates.size(); i++) {
+		
+	}
 }
