efca39d8cf3dedaf7c3ca8422de544c803e51081
zep715 <zep715@gmail.com>
2017-07-14 18:02:25 +0200

    Update prova.cu

--- prova.cu
+++ prova.cu
@@ -13,64 +13,76 @@ using namespace std;
 
 typedef unsigned char byte;
 
+
+
 __global__ void count1itemset(byte *, int, int, int *);
-struct Matrix {
-	int N;
-	int M;
-	byte *matrix;
+
+struct bitmap {
+	int n, m;
+	unsigned int size;
+	unsigned int *bits;
+	bitmap(int rows, int cols) {
+		int bitcols = (cols + 31) / 32;
+		n = rows;
+		m = bitcols;
+		size = n*m;
+		bits = new unsigned int[size];
+		memset(bits, 0, size*sizeof(unsigned int));
+		
+	}
+	~bitmap() {
+		delete[] bits;
+	}
+	void set_bit(int row, int col) {
+		bits[row*m + col / 32] |= (((unsigned int)1) << (31 - col % 32));
+		
+	}
+	bool get_bit(int row, int col) {
+		int i = row*m + col / 32;
+		unsigned int flag = 1;
+		flag = flag << (31 - col % 32);
+		if ((flag&bits[i]) == 0)
+			return false;
+		else
+			return true;
+
+	}
 };
 
-typedef struct Matrix Matrix;
 
-void init_matrix(Matrix *m, int _N, int _M) {
-	m->N = _N;
-	m->M = _M;
-	m->matrix = new byte[_N*_M]();
-}
 
-void delete_matrix(Matrix *m) {
-	delete[] m->matrix;
-	m->matrix = NULL;
-}
-void parse_metadata(char *file, string &csv, int &N, int &M) {
-	ifstream metadata(file, ios::in);
-	metadata >> csv >> N >> M;
-	metadata.close();
-}
+
+
+
 //matrix[i][[j] = matrix[i*M+j]
-void parse_line(string &line, byte *transaction, int row, int cols) {
-	assert(transaction != NULL);
-	stringstream ss(line);
-	unsigned int value;
-	while (ss >> value) {
-		//transaction[value] = 1;
-		transaction[value] = 1;
-		if (ss.peek() == ',')
-			ss.ignore();
-	}
 
-}
 
-void parse_transactions(string &file, Matrix *m, int N, int M) {
+void parse_transactions(string &file,bitmap &t) {
 	ifstream csv(file, ios::in);
 	string line;
 	int i = 0;
 	while (getline(csv, line)) {
-		parse_line(line, &(m->matrix[i*M]), i, M);
+		stringstream ss(line);
+		int value;
+		while (ss >> value) {
+			t.set_bit(i, value);
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
 		i++;
 	}
 	csv.close();
 }
 
-void copy_transactions_to_device(Matrix *m, byte **dev_matrix) {
+void copy_transactions_to_device(bitmap &t, unsigned int **dev_matrix) {
 	cudaError_t status;
-	byte *temp;
-	status = cudaMalloc((void **)&temp, m->M * m->N * sizeof(byte));
+	unsigned int *temp;
+	status = cudaMalloc((void **)&temp, t.n * t.m * sizeof(unsigned int));
 	if (status != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed! dev_matrix");
 		return;
 	}
-	status = cudaMemcpy(temp, m->matrix, m->M * m->N * sizeof(byte), cudaMemcpyHostToDevice);
+	status = cudaMemcpy(temp, t.bits, t.n * t.m * sizeof(unsigned int), cudaMemcpyHostToDevice);
 	if (status != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed! dev_matrix");
 		return;
@@ -81,90 +93,86 @@ void copy_transactions_to_device(Matrix *m, byte **dev_matrix) {
 }
 
 
-
-__global__ void count1itemset(byte *transactions, int N, int M, int *counter) {
+__device__ __host__  unsigned int popcount(unsigned int value) {
+	unsigned int count = 0;
+	while (value > 0) {           // until all bits are zero
+		if ((value & 1) == 1)     // check lower bit
+			count++;
+		value >>= 1;              // shift bits, removing lower bit
+	}
+	return count;
+}
+__global__ void count1itemset(unsigned int *transactions, int N, int M, int *counter) {
 	
-	for (int i = 0; i < N; i++) {
+	int idx = blockDim.x*blockIdx.x + threadIdx.x;
+	for (int i = idx; i < N; i += blockDim.x*gridDim.x) {
 		for (int j = 0; j < M; j++) {
-			if (transactions[i*M + j]) {
-				counter[j]++;
-			}
+			counter[i] += popcount(transactions[i*M + j]);
 		}
 	}
 }	
 
 
 
-void get_1itemset(byte *dev_transactions,  int N, int M, float min_sup) {
-	int *counter = new int[M];
-	int *dev_counter;
-	cudaMalloc((void **)&dev_counter, M*sizeof(int));
-	cudaMemset(dev_counter, 0, M*sizeof(int));
-	count1itemset <<<1,1>>> (dev_transactions, N, M, dev_counter);
-	cudaDeviceSynchronize();
-	cudaMemcpy(counter, dev_counter, M*sizeof(int), cudaMemcpyDeviceToHost);
-	for (int i = 0; i < 2; i++)
-		cout << counter[i] << " ";
-	cout << endl;
-}
 
-__global__ void prova_gpu(byte *transactions, int N, int M, byte *res) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			res[i*M + j] = transactions[i*M + j];
+void get_1itemset(unsigned int *dev_transactions,  int nitems, int M, int ntrans, float min_sup, bitmap &first_itemset) {
+	int *counter = new int[nitems];
+	int *dev_counter;
+	cudaError_t status;
+	status = cudaMalloc((void **)&dev_counter, nitems*sizeof(int));
+	if (status != cudaSuccess) {
+
+		fprintf(stderr, "cudamalloc failed! dev_counter %s\n", cudaGetErrorString(status));
+		goto error;
+	}
+	status = cudaMemset(dev_counter, 0, nitems*sizeof(int));
+	if (status != cudaSuccess) {
+
+		fprintf(stderr, "cudamemset failed! dev_counter %s\n", cudaGetErrorString(status));
+		goto error;
+	}
+	count1itemset <<<5,15>>> (dev_transactions, nitems, M, dev_counter);
+	status = cudaDeviceSynchronize();
+	if (status != cudaSuccess) {
+
+		fprintf(stderr, "cudadevicesynch failed! %s\n", cudaGetErrorString(status));
+		goto error;
+	}
+	status = cudaMemcpy(counter, dev_counter, nitems*sizeof(int), cudaMemcpyDeviceToHost);
+	if (status != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed! counter");
+		goto error;
+	}
+	for (int i = 0; i < nitems; i++) {
+		if (((float)counter[i]) / ntrans >= 0.05) {
+			first_itemset.set_bit(0, i);
+			//cout << i << " ";
 		}
 	}
-}
-void prova(Matrix *m, byte *dev_transactions, int N, int M) {
-	byte *dev_counter;
-	cudaError_t status;
-	status = cudaMalloc((void **)&dev_counter, N*M*sizeof(byte));
-	if (status != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed! dev_matrix");
-		return;
-	}
-	prova_gpu<<<1,1>>> (dev_transactions, N, M, dev_counter);
-	byte *prova = new byte[N*M];
-	status = cudaMemcpy(prova, dev_counter, N*M*sizeof(byte), cudaMemcpyDeviceToHost);
-	if (status != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed! dev_matrix");
-		return;
-	}
-	cout << "----------" << endl;
-	for (int i = 0; i < 20; i++) {
-		for (int j = 0; j <40; j++)
-			cout << (int)prova[i*M + j] << " ";
-		cout << endl;
-	}
-	delete[] prova;
+	cout << endl;
+	error:
 	cudaFree(dev_counter);
+	delete[] counter;
 }
-int main()
-{
-	int N, M;
-	string csv;
-	float min_sup = 0.1;
-	float minconf = 0.5;
 
-	parse_metadata("input.txt", csv, N, M);
-	M++;
-	Matrix matrix;
-	init_matrix(&matrix, N, M);
-	parse_transactions(csv, &matrix,N,M);
-	for (int i = 0; i < 20; i++) {
-		for (int j = 0; j <40; j++)
-			cout << (int)matrix.matrix[i*M + j] << " "; 
-			cout << endl;
-	}
-	byte *dev_transactions;
-	cudaSetDevice(0);
+
+
+int main() {
+	float min_sup = 0.05;
+	ifstream input("input.txt", ios::in);
+	string s;
+	int n, ntrans;
+	input >> s >> n >> ntrans;
+	cout << s << " " << n << " " << ntrans << endl;
+	input.close();
+	ntrans++;
+	bitmap transactions(n, ntrans);
+	bitmap first_itemset(1, n);
+	parse_transactions(s, transactions);
+	unsigned int *dev_transactions;
+	copy_transactions_to_device(transactions, &dev_transactions);
+	get_1itemset(dev_transactions, transactions.n, transactions.m, ntrans, min_sup, first_itemset);
 	
-	copy_transactions_to_device(&matrix, &dev_transactions);
-	//get_1itemset(dev_transactions, N, M, min_sup);
-	prova(&matrix, dev_transactions,  N,  M);
-
 	cudaFree(dev_transactions);
-	delete_matrix(&matrix);
-	return 0;
 }
 
