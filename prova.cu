5b513ba137ae5e6479bf87fa985816d9164861fa
zep715 <zep715@gmail.com>
2017-07-17 17:59:40 +0200

    Update prova.cu

--- prova.cu
+++ prova.cu
@@ -115,7 +115,7 @@ __global__ void count1itemset(unsigned int *transactions, int N, int M, int *cou
 
 
 
-void get_1itemset(unsigned int *dev_transactions,  int nitems, int M, int ntrans, float min_sup, bitmap &first_itemset) {
+void get_1itemset(unsigned int *dev_transactions,  int nitems, int M, int ntrans, float min_sup, bitmap &first_itemset, int &x) {
 	int *counter = new int[nitems];
 	int *dev_counter;
 	cudaError_t status;
@@ -146,7 +146,8 @@ void get_1itemset(unsigned int *dev_transactions,  int nitems, int M, int ntrans
 	for (int i = 0; i < nitems; i++) {
 		if (((float)counter[i]) / ntrans >= 0.05) {
 			first_itemset.set_bit(0, i);
-			//cout << i << " ";
+			x++;
+			cout << i << " ";
 		}
 	}
 	cout << endl;
@@ -156,12 +157,40 @@ void get_1itemset(unsigned int *dev_transactions,  int nitems, int M, int ntrans
 }
 
 
+__global__ void gen_candidates_and_count(unsigned int* dev_transactions, unsigned int* bm, int n, unsigned int *result) {
+	extern __shared__ unsigned int next[];
+	int idx = blockDim.x*blockIdx.x + threadIdx.x;
+	int count = 0;
+	int nn = (n * (n - 1)) / 2;
+	for (int i = idx; i < n; i += blockDim.x*gridDim.x) {
+		for (int j = i + 1; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				next[(i+count)*n+k] = bm[i*n+k] | bm[j*n+k];
+				count++;
+			}
+		}
+	}
+	__syncthreads();
+	memcpy(result, next, nn*sizeof(unsigned int));
+}
+void gen_candidates(unsigned int* d_t, unsigned int *bm, int n) {
+	unsigned int *dev_bm;
+	cudaMalloc((void **)dev_bm, n*sizeof(unsigned int));
+	cudaMemcpy(dev_bm, bm, n*sizeof(unsigned int), cudaMemcpyHostToDevice);
+	int dim = ((n * (n - 1)) / 2)*n;
+	unsigned int*d_result;
+	cudaMalloc((void**)&d_result, dim*sizeof(unsigned int));
+	cudaMemset(d_result, 0, dim*sizeof(unsigned int));
+	gen_candidates_and_count<<<1,n,dim>>>(d_t, dev_bm, n, d_result);
+	unsigned int *result = new unsigned int[dim];
+	cudaMemcpy(result,d_result, dim*sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
+}
 int main() {
-	float min_sup = 0.05;
+	float min_sup = 0.01;
 	ifstream input("input.txt", ios::in);
 	string s;
-	int n, ntrans;
+	int n, ntrans, first_count = 0;
 	input >> s >> n >> ntrans;
 	cout << s << " " << n << " " << ntrans << endl;
 	input.close();
@@ -171,8 +200,16 @@ int main() {
 	parse_transactions(s, transactions);
 	unsigned int *dev_transactions;
 	copy_transactions_to_device(transactions, &dev_transactions);
-	get_1itemset(dev_transactions, transactions.n, transactions.m, ntrans, min_sup, first_itemset);
-	
+	get_1itemset(dev_transactions, transactions.n, transactions.m, ntrans, min_sup, first_itemset, first_count);
+	/*bitmap f(first_count, first_count);
+	for (int i = 0; i < first_count; i++)
+		f.set_bit(i, i);*/
+	unsigned int *f = new unsigned int[first_count*first_count]();
+	for (int i = 0; i < first_count; i++) {
+		f[i*first_count+i] = 1;
+	}
+	gen_candidates(dev_transactions, f, first_count);
+	cout << first_count << endl;
 	cudaFree(dev_transactions);
 }
 
